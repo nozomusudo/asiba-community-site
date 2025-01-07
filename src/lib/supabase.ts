1e140c87cfd2bbc4db7e7adf350546c8269f0bf5
n-sudo-mac <nsadnote22@gmail.com>
2025-01-08 02:15:26 +0900

    fix supabase

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,17 +1,10 @@
-// lib/supabase.ts
 import { createClient } from '@supabase/supabase-js'
 
-let supabase;
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
 
-if (typeof window !== 'undefined') {
-  const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
-  const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
-
-  if (!supabaseUrl || !supabaseAnonKey) {
-    throw new Error('Missing env variables for Supabase')
-  }
-
-  supabase = createClient(supabaseUrl, supabaseAnonKey)
+if (!supabaseUrl || !supabaseAnonKey) {
+  throw new Error('Missing env variables for Supabase')
 }
 
-export { supabase }
+export const supabase = createClient(supabaseUrl, supabaseAnonKey)
