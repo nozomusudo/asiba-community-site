3fe693a6b4a781838fb4a86ba4cd215b5929e226
n-sudo-mac <nsadnote22@gmail.com>
2025-01-07 20:58:45 +0900

    change eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,15 @@
 module.exports = {
-    extends: 'next/core-web-vitals',
+    parser: '@typescript-eslint/parser',
+    plugins: ['@typescript-eslint'],
+    extends: [
+      'next/core-web-vitals',
+      'plugin:@typescript-eslint/recommended'
+    ],
     rules: {
-      "@typescript-eslint/no-explicit-any": "off",
-      "@typescript-eslint/no-unused-vars": "warn",
-      "@next/next/no-img-element": "off",
-      "react-hooks/exhaustive-deps": "warn"
+      '@typescript-eslint/no-unused-vars': 'off',
+      '@typescript-eslint/no-explicit-any': 'off',
+      'react/no-unescaped-entities': 'off',
+      'react-hooks/exhaustive-deps': 'off',
+      '@next/next/no-img-element': 'off'
     }
   }
